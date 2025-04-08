05077920242748923f5ef8c8a2e729d86091285f
Anh Tu Nguyen <tu.anguyen@crossian.com>
2025-04-08 23:20:44 +0700

    meal detail page & not-found page, notFound navagator

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,3 +1,38 @@
-export default function MealDetail() {
-  return <div>Meal Detail</div>;
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default function MealDetail({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) notFound();
+
+  return (
+    <div>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            fill
+            alt="detail"
+            className={classes.image}
+            src={meal?.image}
+          />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal?.title}</h1>
+          <p className={classes.creator}>Name: {meal?.creator}</p>
+          <p className={classes.summary}>Summary: {meal?.summary}</p>
+        </div>
+      </header>
+
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal?.instructions?.replace(/\n/g, "<br />"),
+          }}></p>
+      </main>
+    </div>
+  );
 }
